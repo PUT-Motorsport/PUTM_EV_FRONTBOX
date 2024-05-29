5cd5a45f86a4f2d054349c6979a66a524b0d7549
Adam <Adam_Wasilewski_99@proton.me>
2024-05-29 14:56:06 +0200

    Add steering wheel position sensor

--- Firmware/Core/Inc/AnalogsAbstract.hpp
+++ Firmware/Core/Inc/AnalogsAbstract.hpp
@@ -17,10 +17,14 @@
 class Analog{
 private:
 	static const unsigned number_of_analog_samples = 50;
+	static const uint16_t min_angle = 270;
+	static const uint16_t max_angle = 2850;
+	static constexpr uint16_t sensor_range = max_angle - min_angle;
+
 public:
 	uint16_t steering_position_val_raw[number_of_analog_samples];
 	uint16_t steering_position_val_avg;
-	int16_t get_steering_position();
+	uint16_t get_steering_position();
 };
 
 #endif /* INC_ANALOGSABSTRACT_HPP_ */
