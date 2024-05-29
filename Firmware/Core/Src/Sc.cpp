9a1bd187150097d081a69b8e3247bb77883f1986
Adam <Adam_Wasilewski_99@proton.me>
2024-05-29 14:57:17 +0200

    rework sc data return

--- Firmware/Core/Src/Sc.cpp
+++ Firmware/Core/Src/Sc.cpp
@@ -9,32 +9,38 @@
 
 #include "ScAbstract.hpp"
 
-void SC::update_val()
+uint8_t SC::update_val()
 {
-Sense_BSPD=HAL_GPIO_ReadPin(Sense_BSPD_GPIO_Port, Sense_BSPD_Pin);
-SC_val[0]=Sense_BSPD;
+	Sense_Right=HAL_GPIO_ReadPin(Sense_Right_Kill_GPIO_Port, Sense_Right_Kill_Pin);
+	SC_val[0]=Sense_Right;
 
-Sense_Driver=HAL_GPIO_ReadPin(Sense_Driver_GPIO_Port, Sense_Driver_Pin);
-SC_val[1]=Sense_Driver;
+	Sense_Left=HAL_GPIO_ReadPin(Sense_Left_GPIO_Port, Sense_Left_Pin);
+	SC_val[1]=Sense_Left;
 
-Sense_EBS=HAL_GPIO_ReadPin(Sense_EBS_GPIO_Port, Sense_EBS_Pin);
-SC_val[2]=Sense_EBS;
+	Sense_Driver=HAL_GPIO_ReadPin(Sense_Driver_GPIO_Port, Sense_Driver_Pin);
+	SC_val[2]=Sense_Driver;
 
-Sense_Left=HAL_GPIO_ReadPin(Sense_Left_GPIO_Port, Sense_Left_Pin);
-SC_val[3]=Sense_Left;
+	Sense_Inertia = HAL_GPIO_ReadPin(Sense_Inertia_GPIO_Port, Sense_Inertia_Pin);
+	SC_val[3]=Sense_Inertia;
 
-Sense_Left_Wheel=HAL_GPIO_ReadPin(Sense_Left_Wheel_GPIO_Port, Sense_Left_Wheel_Pin);
-SC_val[4]=Sense_Left_Wheel;
+	Sense_BSPD=HAL_GPIO_ReadPin(Sense_BSPD_GPIO_Port, Sense_BSPD_Pin);
+	SC_val[4]=Sense_BSPD;
 
-Sense_Overtravel=HAL_GPIO_ReadPin(Sense_Overtravel_GPIO_Port, Sense_Overtravel_Pin);
-SC_val[5]=Sense_Overtravel;
+	Sense_Overtravel=HAL_GPIO_ReadPin(Sense_Overtravel_GPIO_Port, Sense_Overtravel_Pin);
+	SC_val[5]=Sense_Overtravel;
 
-//Sense_Right=HAL_GPIO_ReadPin(Sense_Right_GPIO_Port, Sense_Right_Pin);
-//SC_val[6]=Sense_Right;
-SC_val[6]=0;
+	Sense_Right_Wheel=HAL_GPIO_ReadPin(Sense_Right_Wheel_GPIO_Port, Sense_Right_Wheel_Pin);
+	SC_val[6]=Sense_Right_Wheel;
 
-Sense_Right_Wheel=HAL_GPIO_ReadPin(Sense_Right_Wheel_GPIO_Port, Sense_Right_Wheel_Pin);
-SC_val[7]=Sense_Right_Wheel;
+	uint8_t bufor = 0;
+		for(int i = 0; i<=8 ; i++)
+			{
+				if(SC_val[i])
+				{
+					bufor |= 1 << i;
+				}
+			}
+		return bufor;
 }
 
 
